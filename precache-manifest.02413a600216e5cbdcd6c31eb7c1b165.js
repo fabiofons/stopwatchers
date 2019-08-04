e8d5fe9d9634996e211344e397bc64fc13c274ad
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2019-08-04 10:07:33 -0500

    Updates

---
+++ precache-manifest.02413a600216e5cbdcd6c31eb7c1b165.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41dd09a276de2ae46e51460221da8c09",
+    "url": "/stopwatchers/index.html"
+  },
+  {
+    "revision": "e8855469af49dcb97bb3",
+    "url": "/stopwatchers/static/css/main.fef34a25.chunk.css"
+  },
+  {
+    "revision": "163bc3cba0f4fe7179db",
+    "url": "/stopwatchers/static/js/2.51510d46.chunk.js"
+  },
+  {
+    "revision": "e8855469af49dcb97bb3",
+    "url": "/stopwatchers/static/js/main.e46a8759.chunk.js"
+  },
+  {
+    "revision": "4b8b48bc7c7e0209c093",
+    "url": "/stopwatchers/static/js/runtime~main.f4e09a75.js"
+  }
+]);
